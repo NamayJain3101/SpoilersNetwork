f0820beee7ad085d1a9d645beca584edf919c258
namayjain3101 <namayjain.jainnamay@gmail.com>
2021-05-28 21:29:32 +0530

    next

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/namay3101/image/upload",
+        API_KEY: "345263731137914",
+        API_SECRET: "RHoXCVJiA3dS9wDfCdnhtCcWZ74",
+        CLOUD_NAME: "namay3101",
+    }
+}
