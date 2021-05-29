7a58951c1714f27096745d727023ead91c1849b6
namayjain3101 <namayjain.jainnamay@gmail.com>
2021-05-29 08:38:46 +0530

    bug fix

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "production" ? "http://localhost:3000" : "https://spoilers-network.herokuapp.com"
+const baseUrl = process.env.NODE_ENV !== "production" ? "http://localhost:3000" : "https://spoilers-network.herokuapp.com"
 
 module.exports = baseUrl
